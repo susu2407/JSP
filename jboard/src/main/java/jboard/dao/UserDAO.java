46a31687494c669019d03ed79c70b49640cab7ff
susu <tndus159200297@gmail.com>
2025-09-05 15:55:40 +0900

    jsp 최신화

---
+++ jboard/src/main/java/jboard/dao/UserDAO.java
@@ -0,0 +1,85 @@
+package jboard.dao;
+
+import java.util.List;
+
+import jboard.dto.ArticleDTO;
+import jboard.dto.UserDTO;
+import jboard.util.DBHelper;
+import jboard.util.Sql;
+
+public class UserDAO extends DBHelper {
+	
+	private final static UserDAO INSTANCE = new UserDAO();
+	public static UserDAO getInstance() {
+		return INSTANCE;
+	}	
+	private UserDAO() {}
+	
+	
+	public void insert(UserDTO dto) {
+		
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(Sql.INSERT_USER);
+			psmt.setString(1, dto.getUsid());
+			psmt.setString(2, dto.getPass());
+			psmt.setString(3, dto.getUs_name());
+			psmt.setString(4, dto.getNick());
+			psmt.setString(5, dto.getEmail());
+			psmt.setString(6, dto.getHp());
+			psmt.setString(7, dto.getZip());
+			psmt.setString(8, dto.getAddr1());
+			psmt.setString(9, dto.getAddr2());
+			psmt.setString(10, dto.getReg_ip());			
+			
+			psmt.executeUpdate();			
+			closeAll();			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public int selectCount(String col, String value) {
+		try {
+			conn = getConnection();
+			
+			String sql = Sql.SELECT_COUNT;
+			
+			if (col.equals("uid")) {
+				sql += Sql.WHERE_USID;
+			}else if(col.equals("nick")) {
+				sql += Sql.WHERE_NICK;
+			}else if(col.equals("email")) {
+				sql += Sql.WHERE_EMAIL;
+			}else if(col.equals("hp")) {
+				sql += Sql.WHERE_HP;
+			}
+			psmt = conn.prepareStatement(sql.toString());
+			psmt.setString(1, value);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return 0;
+		
+	}
+	
+	
+	public UserDTO select(String usid) {
+		return null;
+	}
+	
+	public List<UserDTO> selectAll() {
+		return null;
+	}
+	
+	public void update(UserDTO dto) {
+		
+	}
+	
+	public void delete(String usid) {
+		
+	}
+}
